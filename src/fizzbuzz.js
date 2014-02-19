cd1a014b48f811abc70d11335acc45d6c96950a3
trikitrok <trikitrok@gmail.com>
2014-02-19 17:13:05 +0100

    Playing a bit more with scopes to generalize substitution rules to any predicate on a number and create the substitute function with just one public factory function

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,28 +1,41 @@
-var makeSubstitution = function (divisor, substitute) {
-  return function substituteNumberBy(acc, number) {
-            if (number % divisor == 0) {
-              acc += substitute;
-            }
-            return acc;
-          };
-}; 
-
-var substitute = function (numbers, substitutions) {
-  function substituteOne (number) {
-    var res = (function f(acc, remainingSubstitutions) {
-      if (remainingSubstitutions.length == 0) {
+var makeSubstitute = function (substitutionDescriptions) {
+  
+  var substituteOne = (function () {
+    function makeSubstitution (pred, replacement) {
+      return function substituteNumber(acc, number) {
+        if ( pred(number) ) {
+          acc += replacement;
+        }
         return acc;
-      } else {
-        acc = remainingSubstitutions[0](acc, number);
-        return f(acc, remainingSubstitutions.slice(1));
-      }
-    }("", substitutions));
+      };
+    }
 
-    if (res != "")
-      return res;
+    var substitutions = [], index;
 
-    return String(number);
-  }
+    for(index = 0; index < substitutionDescriptions.length; index++) {
+      substitutions.push( makeSubstitution(
+                            substitutionDescriptions[index].pred, 
+                            substitutionDescriptions[index].replacement));
+    }
 
-  return numbers.map(substituteOne).join(" ");
+    return function (number) {
+      var res = (function f(acc, remainingSubstitutions) {
+        if (remainingSubstitutions.length == 0) {
+          return acc;
+        } else {
+          acc = remainingSubstitutions[0](acc, number);
+          return f(acc, remainingSubstitutions.slice(1));
+        }
+      }("", substitutions));
+
+      if (res != "")
+        return res;
+
+      return String(number);
+    };
+  }());
+
+  return function (numbers) {
+    return numbers.map(substituteOne).join(" ");
+  };
 };
--- test/SpecFizzBuzz.js
+++ test/SpecFizzBuzz.js
@@ -1,51 +1,76 @@
 describe("FizzBuzz", function () {
-  var fizzBuzzSubstitutionRules;
+  var fizzBuzzSubstitute;
 
   beforeEach(function () {
-    fizzBuzzSubstitutionRules = [makeSubstitution(3, "Fizz"), 
-                                  makeSubstitution(5, "Buzz")];
+    fizzBuzzSubstitute = makeSubstitute([
+      { pred: function(number) {return number % 3 == 0;}, 
+        replacement: "Fizz"},
+      { pred: function(number) {return number % 5 == 0;}, 
+        replacement: "Buzz"}
+    ]);
   });
 
   it("returns empty string for an empty array", function () {
-    expect(substitute([], fizzBuzzSubstitutionRules)).toBe("");
+    expect(fizzBuzzSubstitute([])).toBe("");
   });
 
   it("returns 1 for [1]", function () {
-    expect(substitute([1], fizzBuzzSubstitutionRules)).toBe("1");
+    expect(fizzBuzzSubstitute([1])).toBe("1");
   });
 
   it("returns Fizz for [3]", function () {
-    expect(substitute([3], fizzBuzzSubstitutionRules)).toBe("Fizz");
+    expect(fizzBuzzSubstitute([3])).toBe("Fizz");
   });
 
   it("returns Fizz for [9]", function () {
-    expect(substitute([9], fizzBuzzSubstitutionRules)).toBe("Fizz");
+    expect(fizzBuzzSubstitute([9])).toBe("Fizz");
   });
 
   it("returns Buzz for [5]", function () {
-    expect(substitute([5], fizzBuzzSubstitutionRules)).toBe("Buzz");
+    expect(fizzBuzzSubstitute([5])).toBe("Buzz");
   });
 
   it("returns Buzz for [25]", function () {
-    expect(substitute([25], fizzBuzzSubstitutionRules)).toBe("Buzz");
+    expect(fizzBuzzSubstitute([25])).toBe("Buzz");
   });
 
   it("returns FizzBuzz for [15]", function () {
-    expect(substitute([15], fizzBuzzSubstitutionRules)).toBe("FizzBuzz");
+    expect(fizzBuzzSubstitute([15])).toBe("FizzBuzz");
   });
 
   it("also works with arrays with more than one element", function () {
-    expect(substitute([1, 2, 15], fizzBuzzSubstitutionRules)).toBe("1 2 FizzBuzz");
+    expect(fizzBuzzSubstitute([1, 2, 15])).toBe("1 2 FizzBuzz");
   });
 });
 
 describe("FizzKozz", function () {
 
-  var fizzKozzSubstitutionRules = [makeSubstitution(3, "Fizz"), 
-                                    makeSubstitution(2, "Kozz")];
+  var fizzKozzSubstitute = makeSubstitute([
+      { pred: function(number) {return number % 3 == 0;}, 
+        replacement: "Fizz"},
+      { pred: function(number) {return number % 2 == 0;}, 
+        replacement: "Kozz"}
+    ]);
 
   it("uses substitute with different substitution rules", function () {
-    expect(substitute([1, 2, 4, 6, 15],
-      fizzKozzSubstitutionRules)).toBe("1 Kozz Kozz FizzKozz Fizz");
+    expect(
+      fizzKozzSubstitute([1, 2, 4, 6, 15])).toBe("1 Kozz Kozz FizzKozz Fizz");
+  });
+});
+
+describe("FizzOddSeven", function () {
+  var fizzOddSevenSubstitute = makeSubstitute([
+      { pred: function(number) {return number % 3 == 0;}, 
+        replacement: "Fizz"},
+      { pred: function(number) {return number % 2 == 1;}, 
+        replacement: "Odd"},
+      { pred: function(number) {return number == 7;}, 
+        replacement: "Seven"}
+    ]);
+
+  it("uses substitute with different substitution rules", function () {
+    expect(
+      fizzOddSevenSubstitute(
+        [1, 2, 4, 6, 15, 7])).toBe("Odd 2 4 Fizz FizzOdd OddSeven");
   });
 });
